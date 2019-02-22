9ead9fa469ca1007558ffe0b5bdfb012be54c15d
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-02-22 10:57:12 +0000

    DC-86 - increase limits

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-exec java ${JAVA_OPTS} \
+exec java -XX:+PrintFlagsFinal ${JAVA_OPTS} \
   -Dcom.sun.management.jmxremote.local.only=false \
   -Djava.security.egd=file:/dev/./urandom \
   -jar /app/app.jar
