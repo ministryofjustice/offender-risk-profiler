71fde365cbf3b5f6e339a20c3f99f731fa18598b
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2019-05-10 08:50:53 +0100

    Dc 300 Update risk-profiler dependency versions and graceful shutdown (#32)
    * DC-300 Update risk-profiler dependency versions and graceful shutdown like elite2
    
    * DC-300 Update risk-profiler dependency versions and graceful shutdown like elite2
    
    ensure tests not delayed

--- src/test/java/uk/gov/justice/digital/hmpps/riskprofiler/OffenderRiskProfilerApplicationTest.java
+++ src/test/java/uk/gov/justice/digital/hmpps/riskprofiler/OffenderRiskProfilerApplicationTest.java
@@ -3,9 +3,11 @@ package uk.gov.justice.digital.hmpps.riskprofiler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @SpringBootTest
 public class OffenderRiskProfilerApplicationTest {
 
