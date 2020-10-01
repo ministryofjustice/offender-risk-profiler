aa1ed7fae3e378ddbd8996a2354fd83134db1082
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2020-10-01 17:34:06 +0100

    CAT-660 Increase Extremism Warning logic (#106)

--- src/test/java/uk/gov/justice/digital/hmpps/riskprofiler/services/ExtremismDecisionTreeServiceTest.java
+++ src/test/java/uk/gov/justice/digital/hmpps/riskprofiler/services/ExtremismDecisionTreeServiceTest.java
@@ -1,11 +1,11 @@
 package uk.gov.justice.digital.hmpps.riskprofiler.services;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.justice.digital.hmpps.riskprofiler.dao.PathfinderRepository;
 import uk.gov.justice.digital.hmpps.riskprofiler.datasourcemodel.PathFinder;
 
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class ExtremismDecisionTreeServiceTest {
 
     private static final String OFFENDER_1 = "AB1234A";
@@ -23,19 +23,19 @@ public class ExtremismDecisionTreeServiceTest {
     @Mock
     private PathfinderRepository pathfinderRepo;
 
-    @Before
+    @BeforeEach
     public void setup() {
         service = new ExtremismDecisionTreeService(pathfinderRepo);
     }
 
     @Test
     public void testWhenPathfinderOnFileWithBand1NoPreviousOffences() {
-        PathFinder pathFinder = PathFinder.builder()
+        final PathFinder pathFinder = PathFinder.builder()
                 .nomisId(OFFENDER_1)
                 .pathFinderBanding("BAND 1")
                 .build();
         when(pathfinderRepo.getByKey(eq(OFFENDER_1))).thenReturn(Optional.of(pathFinder));
-        var extremismProfile = service.getExtremismProfile(OFFENDER_1, false);
+        final var extremismProfile = service.getExtremismProfile(OFFENDER_1, false);
 
         Assertions.assertThat(extremismProfile.getProvisionalCategorisation()).isEqualTo("C");
         Assertions.assertThat(extremismProfile.isIncreasedRiskOfExtremism()).isTrue();
@@ -44,12 +44,12 @@ public class ExtremismDecisionTreeServiceTest {
 
     @Test
     public void testWhenPathfinderOnFileWithBand2WithPreviousOffences() {
-        PathFinder pathFinder = PathFinder.builder()
+        final PathFinder pathFinder = PathFinder.builder()
                 .nomisId(OFFENDER_1)
                 .pathFinderBanding("BAND 2")
                 .build();
         when(pathfinderRepo.getByKey(eq(OFFENDER_1))).thenReturn(Optional.of(pathFinder));
-        var extremismProfile = service.getExtremismProfile(OFFENDER_1, true);
+        final var extremismProfile = service.getExtremismProfile(OFFENDER_1, true);
 
         Assertions.assertThat(extremismProfile.getProvisionalCategorisation()).isEqualTo("B");
         Assertions.assertThat(extremismProfile.isIncreasedRiskOfExtremism()).isTrue();
@@ -59,29 +59,29 @@ public class ExtremismDecisionTreeServiceTest {
 
     @Test
     public void testWhenPathfinderOnFileWithBand3() {
-        PathFinder pathFinder = PathFinder.builder()
+        final PathFinder pathFinder = PathFinder.builder()
                 .nomisId(OFFENDER_1)
                 .pathFinderBanding("BAND 3")
                 .build();
         when(pathfinderRepo.getByKey(eq(OFFENDER_1))).thenReturn(Optional.of(pathFinder));
-        var extremismProfile = service.getExtremismProfile(OFFENDER_1, false);
+        final var extremismProfile = service.getExtremismProfile(OFFENDER_1, false);
 
         Assertions.assertThat(extremismProfile.getProvisionalCategorisation()).isEqualTo("C");
-        Assertions.assertThat(extremismProfile.isIncreasedRiskOfExtremism()).isFalse();
+        Assertions.assertThat(extremismProfile.isIncreasedRiskOfExtremism()).isTrue();
         Assertions.assertThat(extremismProfile.isNotifyRegionalCTLead()).isTrue();
     }
 
     @Test
     public void testWhenPathfinderOnFileWithBand4() {
-        PathFinder pathFinder = PathFinder.builder()
+        final PathFinder pathFinder = PathFinder.builder()
                 .nomisId(OFFENDER_1)
                 .pathFinderBanding("BAND 4")
                 .build();
         when(pathfinderRepo.getByKey(eq(OFFENDER_1))).thenReturn(Optional.of(pathFinder));
-        var extremismProfile = service.getExtremismProfile(OFFENDER_1, false);
+        final var extremismProfile = service.getExtremismProfile(OFFENDER_1, false);
 
         Assertions.assertThat(extremismProfile.getProvisionalCategorisation()).isEqualTo("C");
-        Assertions.assertThat(extremismProfile.isIncreasedRiskOfExtremism()).isFalse();
+        Assertions.assertThat(extremismProfile.isIncreasedRiskOfExtremism()).isTrue();
         Assertions.assertThat(extremismProfile.isNotifyRegionalCTLead()).isFalse();
     }
 
@@ -89,7 +89,7 @@ public class ExtremismDecisionTreeServiceTest {
     @Test
     public void testWhenPathfinderNotOnFile() {
         when(pathfinderRepo.getByKey(eq(OFFENDER_1))).thenReturn(Optional.empty());
-        var extremismProfile = service.getExtremismProfile(OFFENDER_1, false);
+        final var extremismProfile = service.getExtremismProfile(OFFENDER_1, false);
 
         Assertions.assertThat(extremismProfile.getProvisionalCategorisation()).isEqualTo("C");
         Assertions.assertThat(extremismProfile.isIncreasedRiskOfExtremism()).isFalse();
