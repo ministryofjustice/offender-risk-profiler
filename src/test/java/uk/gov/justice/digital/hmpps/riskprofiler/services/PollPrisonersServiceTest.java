da431080e1d86dd83cb8c3333ac95db0a769ca76
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2020-12-22 17:37:25 +0000

    CAT-679 Flag PF cases to security (#111)

--- src/test/java/uk/gov/justice/digital/hmpps/riskprofiler/services/PollPrisonersServiceTest.java
+++ src/test/java/uk/gov/justice/digital/hmpps/riskprofiler/services/PollPrisonersServiceTest.java
@@ -33,26 +33,20 @@ public class PollPrisonersServiceTest {
     @Mock
     private EscapeDecisionTreeService escapeDecisionTreeService;
     @Mock
-    private ExtremismDecisionTreeService extremismDecisionTreeService;
-    @Mock
     private PreviousProfileRepository previousProfileRepository;
     @Mock
     private TelemetryClient telemetryClient;
-    @Mock
-    private NomisService nomisService;
     @Mock
     private SQSService sqsService;
 
     private final SocProfile SOC_1 = SocProfile.socBuilder().nomsId(OFFENDER_1).provisionalCategorisation("C").build();
     private final ViolenceProfile VIOLENCE_1 = ViolenceProfile.violenceBuilder().nomsId(OFFENDER_1).provisionalCategorisation("C").build();
     private final EscapeProfile ESCAPE_1 = EscapeProfile.escapeBuilder().nomsId(OFFENDER_1).provisionalCategorisation("C").build();
-    private final ExtremismProfile EXTREMISM_1 = ExtremismProfile.extremismBuilder().nomsId(OFFENDER_1).provisionalCategorisation("C").build();
     private final PreviousProfile PROFILE_1 = PreviousProfile.builder()
             .offenderNo(OFFENDER_1)
             .soc("{\"nomsId\":\"" + OFFENDER_1 + "\",\"provisionalCategorisation\":\"C\",\"transferToSecurity\":false,\"riskType\":\"SOC\"}")
             .violence("{\"nomsId\":\"" + OFFENDER_1 + "\",\"provisionalCategorisation\":\"C\",\"veryHighRiskViolentOffender\":false,\"notifySafetyCustodyLead\":false,\"displayAssaults\":false,\"numberOfAssaults\":0,\"numberOfSeriousAssaults\":0,\"numberOfNonSeriousAssaults\":0,\"riskType\":\"VIOLENCE\"}")
             .escape("{\"nomsId\":\"" + OFFENDER_1 + "\",\"provisionalCategorisation\":\"C\",\"activeEscapeList\":false,\"activeEscapeRisk\":false,\"riskType\":\"ESCAPE\"}")
-            .extremism("{\"nomsId\":\"" + OFFENDER_1 + "\",\"provisionalCategorisation\":\"C\",\"notifyRegionalCTLead\":false,\"increasedRiskOfExtremism\":false,\"riskType\":\"EXTREMISM\"}")
             .executeDateTime(LocalDateTime.now())
             .build();
 
@@ -61,10 +55,8 @@ public class PollPrisonersServiceTest {
         service = new PollPrisonersService(socDecisionTreeService,
                 violenceDecisionTreeService,
                 escapeDecisionTreeService,
-                extremismDecisionTreeService,
                 previousProfileRepository,
                 telemetryClient,
-                nomisService,
                 sqsService);
     }
 
@@ -73,7 +65,6 @@ public class PollPrisonersServiceTest {
         when(socDecisionTreeService.getSocData(OFFENDER_1)).thenReturn(SocProfile.socBuilder().nomsId(OFFENDER_1).transferToSecurity(true).build());
         when(violenceDecisionTreeService.getViolenceProfile(OFFENDER_1)).thenReturn(VIOLENCE_1);
         when(escapeDecisionTreeService.getEscapeProfile(OFFENDER_1)).thenReturn(ESCAPE_1);
-        when(extremismDecisionTreeService.getExtremismProfile(OFFENDER_1, false)).thenReturn(EXTREMISM_1);
         when(previousProfileRepository.findById(OFFENDER_1)).thenReturn(Optional.of(PROFILE_1));
 
         service.pollPrisoner(OFFENDER_1);
@@ -82,14 +73,12 @@ public class PollPrisonersServiceTest {
                 .newProfile(
                         ProfileMessagePayload.builder()
                                 .soc(SocProfile.socBuilder().transferToSecurity(true).nomsId(OFFENDER_1).build())
-                                .extremism(EXTREMISM_1)
                                 .escape(ESCAPE_1)
                                 .violence(VIOLENCE_1)
                                 .build())
                 .oldProfile(
                         ProfileMessagePayload.builder()
                                 .soc(SOC_1)
-                                .extremism(EXTREMISM_1)
                                 .escape(ESCAPE_1)
                                 .violence(VIOLENCE_1)
                                 .build())
@@ -105,7 +94,6 @@ public class PollPrisonersServiceTest {
         when(socDecisionTreeService.getSocData(OFFENDER_1)).thenReturn(SOC_1);
         when(violenceDecisionTreeService.getViolenceProfile(OFFENDER_1)).thenReturn(VIOLENCE_1);
         when(escapeDecisionTreeService.getEscapeProfile(OFFENDER_1)).thenReturn(ESCAPE_1);
-        when(extremismDecisionTreeService.getExtremismProfile(OFFENDER_1, false)).thenReturn(EXTREMISM_1);
         when(previousProfileRepository.findById(OFFENDER_1)).thenReturn(Optional.of(PROFILE_1));
 
         service.pollPrisoner(OFFENDER_1);
@@ -119,7 +107,6 @@ public class PollPrisonersServiceTest {
         when(socDecisionTreeService.getSocData(OFFENDER_1)).thenReturn(SOC_1);
         when(violenceDecisionTreeService.getViolenceProfile(OFFENDER_1)).thenReturn(VIOLENCE_1);
         when(escapeDecisionTreeService.getEscapeProfile(OFFENDER_1)).thenReturn(ESCAPE_1);
-        when(extremismDecisionTreeService.getExtremismProfile(OFFENDER_1, false)).thenReturn(EXTREMISM_1);
         when(previousProfileRepository.findById(OFFENDER_1)).thenReturn(Optional.empty());
 
         service.pollPrisoner(OFFENDER_1);
@@ -134,7 +121,6 @@ public class PollPrisonersServiceTest {
                 profile.getSoc().equals(argument.getSoc())
                         && profile.getViolence().equals(argument.getViolence())
                         && profile.getEscape().equals(argument.getEscape())
-                        && profile.getExtremism().equals(argument.getExtremism())
                         && abs(ChronoUnit.SECONDS.between(profile.getExecuteDateTime(), argument.getExecuteDateTime())) < 2);
     }
 
