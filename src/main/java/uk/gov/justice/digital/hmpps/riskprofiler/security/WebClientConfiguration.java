96195a1b25c05a2822cc6989f86d927c6f35c04f
steverendell <steve.rendell@syscon.net>
2021-03-29 18:05:30 +0100

    CAT-685 Upgrade risk-profiler gradle plugin - Fix event queues
    Also bump dependency versions

--- src/main/java/uk/gov/justice/digital/hmpps/riskprofiler/security/WebClientConfiguration.java
+++ src/main/java/uk/gov/justice/digital/hmpps/riskprofiler/security/WebClientConfiguration.java
@@ -9,12 +9,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -59,14 +57,10 @@ public class WebClientConfiguration {
     @Bean
     public OAuth2AuthorizedClientManager authorizedClientManager(
             final ClientRegistrationRepository clientRegistrationRepository,
-            final OAuth2AuthorizedClientRepository authorizedClientRepository) {
+            final OAuth2AuthorizedClientService authorizedClientService) {
 
-        final OAuth2AuthorizedClientProvider authorizedClientProvider =
-                OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
-
-        final DefaultOAuth2AuthorizedClientManager authorizedClientManager =
-                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository);
-        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+        final OAuth2AuthorizedClientManager authorizedClientManager =
+                new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientService);
 
         return authorizedClientManager;
     }
