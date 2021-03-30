2eb622c99d8f31b4f15e06bd99f848b306e604aa
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2021-03-30 07:38:05 +0100

    CAT-685 Upgrade risk-profiler gradle plugin - Fix event queues (#118)
    Also bump dependency versions

--- src/main/java/uk/gov/justice/digital/hmpps/riskprofiler/queue/LocalstackConfig.java
+++ src/main/java/uk/gov/justice/digital/hmpps/riskprofiler/queue/LocalstackConfig.java
@@ -2,8 +2,11 @@ package uk.gov.justice.digital.hmpps.riskprofiler.queue;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +18,15 @@ public class LocalstackConfig {
     @Bean
     @ConditionalOnProperty(name = "sqs.provider", havingValue = "localstack", matchIfMissing = true)
     @Primary
-    public AmazonSQSAsync awsSqsClient() {
+    @Qualifier("awsClientForEvents")
+    public AmazonSQSAsync awsSqsClient(
+            @Value("${sqs.events.endpoint.url}") final String serviceEndpoint,
+            @Value("${cloud.aws.region.static}") final String region
+    ) {
         var creds = new AnonymousAWSCredentials();
         return AmazonSQSAsyncClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(creds))
-                .withRegion("eu-west-2")
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region))
                 .build();
     }
 }
