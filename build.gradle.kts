f4b1c6a6e12eb457378d19419fb258135ce5c1c5
Alex Hedley <79629950+alex-hedley@users.noreply.github.com>
2022-04-14 13:32:42 +0100

    remove beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4"
   kotlin("plugin.spring") version "1.6.20"
   kotlin("plugin.jpa") version "1.6.20"
 }
