2e99340115eafed0df2706037ecdeb0428a1926c
Alex Hedley <79629950+alex-hedley@users.noreply.github.com>
2022-04-14 12:58:07 +0100

    Updated hmpps.gradle-spring-boot to 4.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
 }
