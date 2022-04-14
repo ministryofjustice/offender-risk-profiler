d801c477467f7beaf2079e38193ca1de3ccc1624
Alex Hedley <79629950+alex-hedley@users.noreply.github.com>
2022-04-14 13:04:11 +0100

    Updated to beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4-beta"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
 }
