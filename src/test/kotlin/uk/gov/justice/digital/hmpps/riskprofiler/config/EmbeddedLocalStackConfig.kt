4e4b1d4cde5aeccc424a0fa240559e3b903de1f2
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-03-28 14:30:59 +0000

    si-828 update local stack port and queue definition

--- src/test/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/config/EmbeddedLocalStackConfig.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/config/EmbeddedLocalStackConfig.kt
@@ -50,9 +50,9 @@ open class EmbeddedLocalStackConfig {
         )
         .withEnv("HOSTNAME_EXTERNAL", "localhost")
         .withEnv("DEFAULT_REGION", "eu-west-2")
-        .waitingFor(
-          Wait.forLogMessage(".*All Ready.*", 1) // .withStartupTimeout(Duration.ofMinutes(10))
-        )
+    //    .waitingFor(
+    //      Wait.forLogMessage(".*All Ready.*", 1) // .withStartupTimeout(Duration.ofMinutes(10))
+    //    )
 
     log.info("Started localstack.")
 
