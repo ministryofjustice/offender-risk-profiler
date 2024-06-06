a0e47d389362e5a22fac429fd024f75f2b8ef184
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-06-06 11:06:11 +0100

    si-956 - added pathfinder and prison api back in to health check

--- src/test/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/integration/IntegrationTestBase.kt
@@ -22,7 +22,7 @@ import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest
 import uk.gov.justice.digital.hmpps.riskprofiler.dao.DataRepositoryFactory
 import uk.gov.justice.digital.hmpps.riskprofiler.integration.mocks.PathfinderMockServer.Companion.pathfinderMockServer
 import uk.gov.justice.digital.hmpps.riskprofiler.integration.mocks.PrisonMockServer.Companion.prisonMockServer
-import uk.gov.justice.digital.hmpps.riskprofiler.integration.mocks.ResourceOAuthMockServer.Companion.oauthMockServer
+import uk.gov.justice.digital.hmpps.riskprofiler.integration.mocks.ResourceOAuthMockServer
 import uk.gov.justice.digital.hmpps.riskprofiler.services.NomisService
 
 import uk.gov.justice.hmpps.sqs.HmppsQueueFactory
@@ -34,7 +34,7 @@ import uk.gov.justice.hmpps.sqs.MissingQueueException
 @Import(IntegrationTestBase.SqsConfig::class, JwtAuthHelper::class)
 //@ExtendWith(OAuthExtension::class)
 @ActiveProfiles("test")
-@TestPropertySource(locations= ["classpath:test.properties"])
+@TestPropertySource(locations = ["classpath:test.properties"])
 abstract class IntegrationTestBase {
 
   @Autowired
@@ -50,31 +50,40 @@ abstract class IntegrationTestBase {
   @BeforeEach
   fun `clear queues`() {
 
-      nomisService.evictSocListAlertsCache("A1234AB")
-      nomisService.evictSocListAlertsCache("A1234AC")
-      nomisService.evictSocListAlertsCache("A1234AE")
-      nomisService.evictSocListAlertsCache("A5015DY")
-      nomisService.evictIncidentsCache("A1234AB")
-      nomisService.evictIncidentsCache("A1234AC")
-      nomisService.evictEscapeListAlertsCache("A1234AB")
-      nomisService.evictEscapeListAlertsCache("A1234AC")
+    prisonMockServer.stubPing()
+    pathfinderMockServer.stubPing()
 
-   //   prisonMockServer.resetAll()
-  //    oauthMockServer.resetAll()
-  //    pathfinderMockServer.resetAll()
-      await until { allFilesLoaded() }
+    nomisService.evictSocListAlertsCache("A1234AB")
+    nomisService.evictSocListAlertsCache("A1234AC")
+    nomisService.evictSocListAlertsCache("A1234AE")
+    nomisService.evictSocListAlertsCache("A5015DY")
+    nomisService.evictIncidentsCache("A1234AB")
+    nomisService.evictIncidentsCache("A1234AC")
+    nomisService.evictEscapeListAlertsCache("A1234AB")
+    nomisService.evictEscapeListAlertsCache("A1234AC")
 
-    riskProfilerChangeSqsClientSpy.purgeQueue(PurgeQueueRequest.builder().queueUrl(riskProfilerChangeQueueUrl).build()).get()
-    riskProfilerChangeSqsDlqClientSpy.purgeQueue(PurgeQueueRequest.builder().queueUrl(riskProfilerChangeDlqUrl).build()).get()
- }
+    //   prisonMockServer.resetAll()
+    //    oauthMockServer.resetAll()
+    //    pathfinderMockServer.resetAll()
+    await until { allFilesLoaded() }
 
-  private val riskProfilerChangeQueue by lazy { hmppsQueueService.findByQueueId("riskprofilechangequeue") ?: throw MissingQueueException("HmppsQueue outboundqueue not found") }
+    riskProfilerChangeSqsClientSpy.purgeQueue(PurgeQueueRequest.builder().queueUrl(riskProfilerChangeQueueUrl).build())
+      .get()
+    riskProfilerChangeSqsDlqClientSpy.purgeQueue(PurgeQueueRequest.builder().queueUrl(riskProfilerChangeDlqUrl).build())
+      .get()
+  }
+
+  private val riskProfilerChangeQueue by lazy {
+    hmppsQueueService.findByQueueId("riskprofilechangequeue")
+      ?: throw MissingQueueException("HmppsQueue outboundqueue not found")
+  }
 
   protected val riskProfilerChangeQueueUrl by lazy { riskProfilerChangeQueue.queueUrl }
   protected val riskProfilerChangeDlqUrl by lazy { riskProfilerChangeQueue.dlqUrl as String }
 
   fun HmppsSqsProperties.riskProfilerChangeQueueConfig() =
-    queues["riskprofilechangequeue"] ?: throw MissingQueueException("riskprofilechangequeue has not been loaded from configuration properties")
+    queues["riskprofilechangequeue"]
+      ?: throw MissingQueueException("riskprofilechangequeue has not been loaded from configuration properties")
 
   @SpyBean
   @Qualifier("riskprofilechangequeue-sqs-client")
@@ -96,7 +105,10 @@ abstract class IntegrationTestBase {
   @Autowired
   lateinit var webTestClient: WebTestClient
 
-  internal fun HttpHeaders.authToken(user: String?, roles: List<String> = listOf("ROLE_QUEUE_ADMIN", "ROLE_RISK_PROFILER")) {
+  internal fun HttpHeaders.authToken(
+    user: String?,
+    roles: List<String> = listOf("ROLE_QUEUE_ADMIN", "ROLE_RISK_PROFILER"),
+  ) {
     this.setBearerAuth(
       jwtAuthHelper.createJwt(
         subject = user,
@@ -117,14 +129,16 @@ abstract class IntegrationTestBase {
       @Qualifier("riskprofilechangequeue-sqs-dlq-client") riskProfileChangeQueueSqsDlqClient: SqsAsyncClient,
     ): SqsAsyncClient =
       with(hmppsSqsProperties) {
-        val config = queues["riskprofilechangequeue"] ?: throw MissingQueueException("HmppsSqsProperties config for hmppsriskprofilechangequeue not found")
+        val config = queues["riskprofilechangequeue"]
+          ?: throw MissingQueueException("HmppsSqsProperties config for hmppsriskprofilechangequeue not found")
         hmppsQueueFactory.createSqsAsyncClient(config, hmppsSqsProperties, riskProfileChangeQueueSqsDlqClient)
       }
 
     @Bean("riskprofilechangequeue-sqs-dlq-client")
     fun riskProfileChangeQueueSqsDlqClient(hmppsSqsProperties: HmppsSqsProperties): SqsAsyncClient =
       with(hmppsSqsProperties) {
-        val config = queues["riskprofilechangequeue"] ?: throw MissingQueueException("HmppsSqsProperties config for outboundqueue not found")
+        val config = queues["riskprofilechangequeue"]
+          ?: throw MissingQueueException("HmppsSqsProperties config for outboundqueue not found")
         hmppsQueueFactory.createSqsAsyncDlqClient(config, hmppsSqsProperties)
       }
   }
