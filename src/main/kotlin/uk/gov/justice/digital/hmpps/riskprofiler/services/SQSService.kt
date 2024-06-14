a0fc57b7565d80a75d99e30b7b4793e4fb59ced8
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-06-14 16:03:26 +0100

    si-956 - update sqs properties after cal with Steve Rendell

--- src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/services/SQSService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/services/SQSService.kt
@@ -21,7 +21,7 @@ class SQSService(
 
 
   init {
-    val riskProfileChangeQueue = hmppsQueueService.findByQueueId("risk_profiler_change") ?: throw MissingQueueException("Could not find topic riskprofilechangequeue")
+    val riskProfileChangeQueue = hmppsQueueService.findByQueueId("riskprofilechangequeue") ?: throw MissingQueueException("Could not find topic riskprofilechangequeue")
 
     riskProfileChangeQueueSqsClient = riskProfileChangeQueue.sqsClient
   }
