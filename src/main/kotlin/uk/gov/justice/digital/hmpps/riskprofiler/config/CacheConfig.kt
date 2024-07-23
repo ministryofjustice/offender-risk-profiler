93847b8d760a70ec50a4b0aed330250cd420def0
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-07-23 15:11:18 +0100

    using baledung example for redis

--- src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/config/CacheConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/config/CacheConfig.kt
@@ -1,67 +1,23 @@
 package uk.gov.justice.digital.hmpps.riskprofiler.config
 
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.cache.RedisCacheConfiguration
-import org.springframework.data.redis.cache.RedisCacheManager
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
-import uk.gov.justice.digital.hmpps.riskprofiler.model.PrisonSupported
-import java.awt.print.Book
-import java.time.Duration
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
-
+@Configuration
+@EnableRedisRepositories
 class CacheConfig {
-  @Value("\${spring.redis.cache.timeout-days}")
-  private val timeoutDays = 0
-
-  @Value("\${spring.data.redis.host}")
-  private val server: String? = null
-
-  @Value("\${spring.data.redis.port}")
-  private val port = 0
-
-  @Value("\${spring.data.redis.password}")
-  private val password: String? = null
-
-  @Value("\${spring.data.redis.ssl.enabled}")
-  private val ssl = false
-
-  @Value("\${spring.data.redis.client-name}")
-  private val clientName: String? = null
-
-  /*
   @Bean
-  fun jedisConnectionFactory(): JedisConnectionFactory {
-    val redisConfig = RedisStandaloneConfiguration(server!!, port)
-    redisConfig.setPassword(password)
-    val jedisClientConfigurationBuilder = JedisClientConfiguration.builder()
-    if (ssl) {
-      jedisClientConfigurationBuilder.useSsl()
-    }
-    jedisClientConfigurationBuilder.usePooling()
-    jedisClientConfigurationBuilder.clientName(clientName!!)
-    return JedisConnectionFactory(redisConfig, jedisClientConfigurationBuilder.build())
+  fun redisConnectionFactory(): LettuceConnectionFactory {
+    return LettuceConnectionFactory()
   }
 
-*/
-/*
   @Bean
-  fun redisConnectionFactory() : RedisConnectionFactory {
-    val redisConnectionFactory = RedisConnectionFactory()
-  }
-*/
-  @Bean
-  fun redisTemplate(connectionFactory: RedisConnectionFactory?): RedisTemplate<Long, PrisonSupported> {
-
-    val template = RedisTemplate<Long, PrisonSupported>()
-    template.connectionFactory = connectionFactory
-    // Add some specific configuration here. Key serializers, etc.
+  fun redisTemplate(): RedisTemplate<*, *> {
+    val template = RedisTemplate<ByteArray, ByteArray>()
+    template.connectionFactory = redisConnectionFactory()
     return template
   }
 }
