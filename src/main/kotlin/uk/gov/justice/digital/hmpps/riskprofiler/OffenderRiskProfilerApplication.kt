8bf55c95f9c1f81d300c4ce7be1b93f7d7c82faf
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-07-24 17:30:20 +0100

    Enable camel routes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/OffenderRiskProfilerApplication.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/OffenderRiskProfilerApplication.kt
@@ -1,11 +1,13 @@
 package uk.gov.justice.digital.hmpps.riskprofiler
 
-import org.springframework.boot.SpringApplication
+import jakarta.annotation.PreDestroy
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+
 
 @SpringBootApplication
 @EnableRedisRepositories(basePackages = ["uk.gov.justice.digital.hmpps.riskprofiler.cache"])
@@ -15,4 +17,16 @@ class OffenderRiskProfilerApplication
 
 fun main(args: Array<String>) {
   runApplication<OffenderRiskProfilerApplication>(*args)
+}
+
+@PreDestroy
+fun onExit() {
+  val log = LoggerFactory.getLogger(OffenderRiskProfilerApplication::class.java)
+  log.info("###STOPing###")
+  try {
+    Thread.sleep((5 * 1000).toLong())
+  } catch (e: InterruptedException) {
+    log.error("", e)
+  }
+  log.info("###STOP FROM THE LIFECYCLE###")
 }
