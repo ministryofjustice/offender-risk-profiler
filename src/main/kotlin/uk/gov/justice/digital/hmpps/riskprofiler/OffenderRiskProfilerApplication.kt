a2d51a59ffe7799142dc8d9d07aa6bc6f5011902
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-07-24 16:45:06 +0100

    Enable caching as before

--- src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/OffenderRiskProfilerApplication.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/OffenderRiskProfilerApplication.kt
@@ -3,12 +3,14 @@ package uk.gov.justice.digital.hmpps.riskprofiler
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
 @EnableRedisRepositories(basePackages = ["uk.gov.justice.digital.hmpps.riskprofiler.cache"])
 @EnableJpaRepositories(basePackages = ["uk.gov.justice.digital.hmpps.riskprofiler.dao"])
+@EnableCaching
 class OffenderRiskProfilerApplication
 
 fun main(args: Array<String>) {
