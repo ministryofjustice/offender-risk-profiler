80a1611cd4229001c80c6d071fdb97acce26d026
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-07-29 12:44:28 +0100

    Enable camel routes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/camel/BasicRoute.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/riskprofiler/camel/BasicRoute.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.riskprofiler.camel
 
 import org.apache.camel.builder.RouteBuilder
+import org.springframework.stereotype.Component
 
+@Component
 class BasicRoute : RouteBuilder() {
   override fun configure() {
     from("direct:a").to("direct:b");
