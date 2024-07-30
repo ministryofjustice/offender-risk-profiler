13ecf2374e296df073481dba6bdf663cef1ee029
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-07-30 10:10:39 +0100

    Enable camel routes

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
   implementation("com.microsoft.azure:applicationinsights-spring-boot-starter:2.6.4")
   implementation("com.microsoft.azure:applicationinsights-logging-logback:2.6.4")
 
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.32.0")
+  ///implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.32.0")
 
   /*
     implementation("org.apache.camel:camel-management:4.6.0")
