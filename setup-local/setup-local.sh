2e261043fd9bbfd5bc5888fd1941fc2123d061c8
cruffymoj <125977693+cruffymoj@users.noreply.github.com>
2024-08-02 11:13:11 +0100

    si-1143 - add docker-compose and script to create s3 buckets, sqsq queues and s3 bucket csv data

---
+++ setup-local/setup-local.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# --------------------------------------------------------------------------
+#
+#   setup local s3 and sqs in localstack,called by docker-compose-local task
+#
+#  -------------------------------------------------------------------------
+
+sleep 5 # we should have better way, srlsy it is not 90s ;[]
+
+set -e
+export TERM=ansi
+export AWS_ACCESS_KEY_ID=foobar
+export AWS_SECRET_ACCESS_KEY=foobar
+export AWS_DEFAULT_REGION=eu-west-2
+
+echo 'Starting localstack bucket setup'
+
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name events-dl
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name events
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name riskprofilechangequeue
+
+aws --endpoint-url=http://localhost:4566 s3 mb s3://testbucket
+
+aws --endpoint-url=http://localhost:4566 s3 cp /etc/localstack/init/ready.d/buckets/VIPER-Dummy.csv s3://testbucket/viper/VIPER-Dummy.csv
+aws --endpoint-url=http://localhost:4566 s3 cp /etc/localstack/init/ready.d/buckets/OCGM-Dummy.csv s3://testbucket/ocgm/OCGM-Dummy.csv
+aws --endpoint-url=http://localhost:4566 s3 cp /etc/localstack/init/ready.d/buckets/OCG-Dummy.csv s3://testbucket/ocg/OCG-Dummy.csv
+aws --endpoint-url=http://localhost:4566 s3 cp /etc/localstack/init/ready.d/buckets/PRAS-Dummy.csv s3://testbucket/pras/PRAS-Dummy.csv
+
+echo 'Finished localstack bucket setup'
+
+aws --endpoint-url=http://localstack:4566 s3 ls s3://testbucket --recursive
+
+echo 'Listing all queues in Localstack'
+
+aws --endpoint-url=http://localstack:4566 sqs list-queues
+
+aws configure list
+
+echo All Ready
