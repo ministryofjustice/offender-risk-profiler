4df43968a3b1838d0735f692e257160f594616bc
raghavender.tangallapally <raghavender.tangallapally@justice.gov.uk>
2025-03-20 22:20:57 +0000

    SI-1409 - security scan stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.0")
 
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
